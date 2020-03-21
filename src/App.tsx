f703868b0b831fee990f5923429d253ac55630ca
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2020-03-21 12:15:53 +0100

    feat: create routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ErrorPage from './pages/ErrorPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
-function App() {
-  return <div>Selam</div>;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        {/* <Route exact path="/" render={() => <Redirect to="/home" />} /> */}
+        <Route path="/home" component={HomePage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={RegisterPage} />
+        <Route path="/error" component={ErrorPage} />
+
+        <Redirect to="/home" exact path="/" />
+        <Redirect to="/error"></Redirect>
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
