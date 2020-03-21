92df0344501651ccf3ddb6fd5183dced97839f0f
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2020-03-21 15:04:19 +0100

    feat: implement redux and implement notification-container

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,27 @@ import HomePage from './pages/HomePage';
 import ErrorPage from './pages/ErrorPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import NotificationContainer from './components/Notification/NotificationContainer';
+import { Provider } from 'react-redux';
+import { configureStore } from './redux/store';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        {/* <Route exact path="/" render={() => <Redirect to="/home" />} /> */}
-        <Route path="/home" component={HomePage} />
-        <Route path="/login" component={LoginPage} />
-        <Route path="/register" component={RegisterPage} />
-        <Route path="/error" component={ErrorPage} />
+    <Provider store={configureStore()}>
+      <NotificationContainer />
+      <BrowserRouter>
+        <Switch>
+          {/* <Route exact path="/" render={() => <Redirect to="/home" />} /> */}
+          <Route path="/home" component={HomePage} />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/register" component={RegisterPage} />
+          <Route path="/error" component={ErrorPage} />
 
-        <Redirect to="/home" exact path="/" />
-        <Redirect to="/error"></Redirect>
-      </Switch>
-    </BrowserRouter>
+          <Redirect to="/home" exact path="/" />
+          <Redirect to="/error"></Redirect>
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
