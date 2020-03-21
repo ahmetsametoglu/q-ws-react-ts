d7506465920b777d00f875b613e2f04c0c5d24a8
ahmetsametoglu.fr@gmail.com <ahmetsametoglu.fr@gmail.com>
2020-03-21 12:17:04 +0100

    feat: create home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useState, Fragment, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import LoginRouteButtons from '../container/LoginRouteButtons';
+
+type State = { username?: string; password?: string };
 
 const HomePage = () => {
-  return <div>Home Page</div>;
+  const [state, setState] = useState<State>({});
+  const history = useHistory();
+
+  useEffect(() => {
+    console.log('[home]: useEffect component mount');
+
+    return () => {
+      console.log('[home]: useEffect component destroy');
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log('[home]: useEffect state changed', state);
+    return () => {
+      console.log('[home]: useEffect state changed return', state);
+    };
+  }, [state]);
+
+  const changeName = () => {
+    setState({ ...state, username: 'serdar' });
+  };
+
+  const goToLoginPage = () => {
+    history.push('/login');
+  };
+
+  return (
+    <Fragment>
+      <h1>Home Page</h1>
+      <h4>{state?.username}</h4>
+      <h4>{state?.password}</h4>
+      <button onClick={changeName}>degistir</button>
+      <button onClick={goToLoginPage}>Go Login Page</button>
+
+      <LoginRouteButtons />
+    </Fragment>
+  );
 };
 
 export default HomePage;
